cce4ec50ec62778eaa81f14f15ef22e1c62304e8
kpmeza <kpmeza70@misena.edu.co>
2024-02-20 19:46:24 -0500

    first commit

---
+++ procesos.js
@@ -0,0 +1,17 @@
+function mostrar(){
+    document.getElementById("info").innerHTML = "<h1> producto 2</h1>"
+    +"<h3>Nombre: Manilla Nukak Makú </h3>"
+    +"<h3>Técnica: Tejeduría </h3>"
+    +"<h3>Materia Prima: Fibra de cumare torcida </h3>"
+    +"<h3>Precio: $6.000</h3>";
+    
+}
+
+function mostrar2(){
+    document.getElementById("info2").innerHTML = "<h1> producto 3</h1>"
+    +"<h3>Nombre: Ají Molido </h3>"
+    +"<h3>Técnica: Ninguna </h3>"
+    +"<h3>Materia Prima:Ají 100% Natural </h3>"
+    +"<h3>Precio:  $10.000</h3>";
+    
+}
